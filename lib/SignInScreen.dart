088a55d76feb349bef29e082bf797353f49ba35c
Yazan Al Sedawiey <yazan.alsedawiey@gmail.com>
2023-05-15 23:43:29 +0300

    Edit (Login->signin)

---
+++ lib/SignInScreen.dart
@@ -0,0 +1,428 @@
+import 'package:car1/ForgetPassword.dart';
+import 'package:car1/SignUp.dart';
+import 'package:car1/User/Home/HomePageUser.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  static const routeName = '/SignInScreen';
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  late double _height;
+  late double _width;
+  late double _pixelRatio;
+  late bool _large;
+  late bool _medium;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> _key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    _height = MediaQuery.of(context).size.height;
+    _width = MediaQuery.of(context).size.width;
+    _pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    _large = ResponsiveWidget.isScreenLarge(_width, _pixelRatio);
+    _medium = ResponsiveWidget.isScreenMedium(_width, _pixelRatio);
+    return Material(
+      child: Container(
+        height: _height,
+        width: _width,
+        padding: EdgeInsets.only(bottom: 5),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              clipShape(),
+              welcomeTextRow(),
+              signInTextRow(),
+              form(),
+              forgetPassTextRow(),
+              SizedBox(height: _height / 12),
+              button(),
+              signUpTextRow(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget clipShape() {
+    return Stack(
+      children: <Widget>[
+        Opacity(
+          opacity: 0.75,
+          child: ClipPath(
+            clipper: CustomShapeClipper(),
+            child: Container(
+              height: _large
+                  ? _height / 4
+                  : (_medium ? _height / 3.75 : _height / 3.5),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 241, 240, 238),
+                    Color.fromARGB(195, 24, 1, 9)
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Opacity(
+          opacity: 0.8,
+        ),
+        Container(
+          alignment: Alignment.bottomCenter,
+          margin: EdgeInsets.only(
+              top: _large
+                  ? _height / 30
+                  : (_medium ? _height / 25 : _height / 20)),
+          child: Image.asset(
+            'photo/background.png',
+            height: _height / 4.5,
+            width: _width / 3.5,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget welcomeTextRow() {
+    return Container(
+      margin: EdgeInsets.only(left: _width / 20, top: _height / 100),
+      child: Row(
+        children: <Widget>[
+          Text(
+            "Welcome",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: _large ? 60 : (_medium ? 50 : 40),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget signInTextRow() {
+    return Container(
+      margin: EdgeInsets.only(left: _width / 15.0),
+      child: Row(
+        children: <Widget>[
+          Text(
+            "Sign in to your account",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: _large ? 20 : (_medium ? 17.5 : 15),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget form() {
+    return Container(
+      margin: EdgeInsets.only(
+          left: _width / 12.0, right: _width / 12.0, top: _height / 15.0),
+      child: Form(
+        key: _key,
+        child: Column(
+          children: <Widget>[
+            emailTextFormField(),
+            SizedBox(height: _height / 40.0),
+            passwordTextFormField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailTextFormField() {
+    return CustomTextField(
+      keyboardType: TextInputType.emailAddress,
+      textEditingController: emailController,
+      icon: Icons.email,
+      hint: "Email ID",
+    );
+  }
+
+  Widget passwordTextFormField() {
+    return CustomTextField(
+      keyboardType: TextInputType.emailAddress,
+      textEditingController: passwordController,
+      icon: Icons.lock,
+      obscureText: true,
+      hint: "Password",
+    );
+  }
+
+  Widget forgetPassTextRow() {
+    return Container(
+      margin: EdgeInsets.only(top: _height / 40.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Forgot your password?",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: _large ? 15 : (_medium ? 15 : 12)),
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ForgetPassword()));
+            },
+            child: Text(
+              "Forget Passowrd",
+              style:
+                  TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget button() {
+    return RaisedButton(
+      elevation: 0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      onPressed: () {
+        print("Routing to your account");
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text('Login Successful'),
+          duration: Duration(
+            milliseconds: 750,
+          ),
+        ));
+      },
+      textColor: Colors.white,
+      padding: EdgeInsets.all(0.0),
+      child: Container(
+        alignment: Alignment.center,
+        width: _large ? _width / 4 : (_medium ? _width / 3.75 : _width / 3.5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          gradient: LinearGradient(
+            colors: <Color>[
+              Color.fromARGB(255, 241, 239, 236),
+              Color.fromARGB(255, 24, 0, 8)
+            ],
+          ),
+        ),
+        padding: const EdgeInsets.all(12.0),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => HomePageUser()));
+          },
+          child: Text('SIGN IN',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: _large ? 14 : (_medium ? 15 : 12),
+                  fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+
+  Widget signUpTextRow() {
+    return Container(
+      margin: EdgeInsets.only(top: _height / 120.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Don't have an account?",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: _large ? 14 : (_medium ? 15 : 12)),
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SignupPage()));
+
+              // print("Routing to Sign up screen");
+            },
+            child: Text(
+              "Sign up",
+              style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white,
+                  fontSize: _large ? 19 : (_medium ? 17 : 15)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+//==================
+// custom shape
+//==================
+class CustomShapeClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0.0, size.height - 70);
+
+    var firstEndPoint = Offset(size.width * .5, size.height - 30.0);
+    var firstControlpoint = Offset(size.width * 0.25, size.height - 50.0);
+    path.quadraticBezierTo(firstControlpoint.dx, firstControlpoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+
+    var secondEndPoint = Offset(size.width, size.height - 50.0);
+    var secondControlPoint = Offset(size.width * .75, size.height - 10);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => true;
+}
+
+class CustomShapeClipper2 extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0.0, size.height - 20);
+
+    var firstEndPoint = Offset(size.width * .5, size.height - 30.0);
+    var firstControlpoint = Offset(size.width * 0.25, size.height - 50.0);
+    path.quadraticBezierTo(firstControlpoint.dx, firstControlpoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+
+    var secondEndPoint = Offset(size.width, size.height - 5);
+    var secondControlPoint = Offset(size.width * .75, size.height - 20);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => true;
+}
+
+//==================
+//  Custom AppBar
+//==================
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Material(
+      child: Container(
+        height: height / 10,
+        width: width,
+        padding: EdgeInsets.only(left: 15, top: 25),
+        decoration: BoxDecoration(
+          gradient:
+              LinearGradient(colors: [Colors.orange[200]!, Colors.pinkAccent]),
+        ),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                ),
+                onPressed: () {
+                  print("pop");
+                  Navigator.of(context).pop();
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//=====================
+// Responsive Widget
+//=====================
+class ResponsiveWidget {
+  static bool isScreenLarge(double width, double pixel) {
+    return width * pixel >= 1440;
+  }
+
+  static bool isScreenMedium(double width, double pixel) {
+    return width * pixel < 1440 && width * pixel >= 1080;
+  }
+
+  static bool isScreenSmall(double width, double pixel) {
+    return width * pixel <= 720;
+  }
+}
+
+//======================
+//Custom TextField
+//=====================
+
+class CustomTextField extends StatelessWidget {
+  final String hint;
+  final TextEditingController textEditingController;
+  final TextInputType keyboardType;
+  final bool obscureText;
+  final IconData icon;
+  late double _width;
+  late double _pixelRatio;
+  late bool large;
+  late bool medium;
+
+  CustomTextField({
+    required this.hint,
+    required this.textEditingController,
+    required this.keyboardType,
+    required this.icon,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    _width = MediaQuery.of(context).size.width;
+    _pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    large = ResponsiveWidget.isScreenLarge(_width, _pixelRatio);
+    medium = ResponsiveWidget.isScreenMedium(_width, _pixelRatio);
+    return Material(
+      borderRadius: BorderRadius.circular(30.0),
+      elevation: large ? 12 : (medium ? 10 : 8),
+      child: TextFormField(
+        controller: textEditingController,
+        keyboardType: keyboardType,
+        cursorColor: Colors.orange[200]!,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon, color: Colors.orange[200]!, size: 20),
+          hintText: hint,
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              borderSide: BorderSide.none),
+        ),
+      ),
+    );
+  }
+}
